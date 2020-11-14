2bde09c86180c6575cfd48b5177255a4f3bb1ecc
Nafis Hasnain <nafishasnain0@gmail.com>
2020-11-14 14:55:41 -0500

    Added styles, will add butto and border

--- src/pages/descriptions/MockData.js
+++ src/pages/descriptions/MockData.js
@@ -2,23 +2,30 @@ const MockData = () => {
     return([
         {
             teamName: "Exec",
-            lastUpdated: "Nov 13"
+            lastUpdated: "Nov 13",
+            key: 1
         },
         {
             teamName: "Mechanical",
-            lastUpdated: "Nov 13"
+            lastUpdated: "Nov 13",
+            key: 2
         },
         {
             teamName: "Electrical",
-            lastUpdated: "Nov 13"
+            lastUpdated: "Nov 13",
+            key: 3
         },
         {
             teamName: "Software",
-            lastUpdated: "Nov 13"
+            lastUpdated: "Nov 13",
+            key: 4
         },
         {
             teamName: "Web",
-            lastUpdated: "Nov 13"
+            lastUpdated: "Nov 13",
+            key: 5
         }
     ])
 }
+
+export default MockData
--- src/pages/descriptions/TeamDesc.js
+++ src/pages/descriptions/TeamDesc.js
@@ -1,10 +1,41 @@
-import React from 'react';
-import styled from 'styled-components';
+import React,{useState} from 'react';
+import styled from 'styled-components'
+import MockData from './MockData';
 
-const TeamDesc = () => (
-    <div>
-        Stuff
-    </div>
-)
+const Spann = styled.nav`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
+
+const Container = styled.div`
+    font-family: 'IBM Plex Sans';
+    width: 29%;
+    margin-left: 10%;
+    color: #2B2B2B;
+    h1{
+        font-style:italic;
+        padding-bottom: 0;
+        margin-bottom: 0;
+    }
+`
+
+function TeamDesc(){
+    const [list, setList] = useState(MockData());
+    
+    return(
+    <Container>
+        <h1>All Teams</h1>
+        {list.map(item =>{
+                return(
+                <Spann>
+                    <h2>{item.teamName}</h2> 
+                    <div>{item.lastUpdated}</div>
+                    
+                </Spann>)}
+                )}
+    </Container>
+    )
+}
 
 export default TeamDesc;
