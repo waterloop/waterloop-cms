14097ce5f042580986512ca50752de22991ed402
Nafis Hasnain <nafishasnain0@gmail.com>
2020-11-13 23:51:24 -0500

    Added mock team data for mapping

---
+++ src/pages/descriptions/MockData.js
@@ -0,0 +1,24 @@
+const MockData = () => {
+    return([
+        {
+            teamName: "Exec",
+            lastUpdated: "Nov 13"
+        },
+        {
+            teamName: "Mechanical",
+            lastUpdated: "Nov 13"
+        },
+        {
+            teamName: "Electrical",
+            lastUpdated: "Nov 13"
+        },
+        {
+            teamName: "Software",
+            lastUpdated: "Nov 13"
+        },
+        {
+            teamName: "Web",
+            lastUpdated: "Nov 13"
+        }
+    ])
+}
