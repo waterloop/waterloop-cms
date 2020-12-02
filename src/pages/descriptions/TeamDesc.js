3ceaa721ab9854b8790d377f0e36f8b17cb26271
Nafis Hasnain <nafishasnain0@gmail.com>
2020-12-02 18:26:05 -0500

    Updated table visuals

--- src/pages/descriptions/TeamDesc.js
+++ src/pages/descriptions/TeamDesc.js
@@ -3,6 +3,8 @@ import styled from 'styled-components'
 import MockData from './MockData';
 import Add from './add.svg'
 import Edit from './edit.svg'
+import ToggleUp from './up.svg'
+import ToggleDown from './down.svg'
 
 const OuterContainer = styled.div`
  font-family: 'IBM Plex Sans';   
@@ -12,7 +14,15 @@ const Spann = styled.nav`
     display: flex;
     justify-content: space-between;
     align-items: center;
-    width: 25%;
+    width: 35%;
+    h2{
+        font-size: 18px;
+        padding-left: 5px;
+    }
+    .date{
+        font-size: 15px;
+        padding-left: 5px;
+    }
 `;
 
 const Container = styled.div`
@@ -35,6 +45,11 @@ const Container = styled.div`
     }
 `
 
+const InnerContainer = styled.div`
+    border: 2px solid #C4C4C4;
+    border-radius: 10px 10px 0px 0px;
+`
+
 const OuterSpan = styled.div`
     display: flex;
     justify-content: space-between;
@@ -44,17 +59,12 @@ const OuterSpan = styled.div`
         border: none;
         cursor: pointer;
         border: 1px solid #FED95A;
-        padding: 0 0.5rem;
-        transition: all 0.3s ease;
-        &:hover{
-            background: rgb(189, 181, 181);
-        };
         border-radius: 15px;
-        text-align: center;font-family: IBM Plex Sans;
+        text-align: center;
+        font-family: IBM Plex Sans;
         font-style: normal;
         font-weight: bold;
         font-size: 15px;
-        line-height: 23px;
         display: flex;
         align-items: center;
         color: #2B2B2B;
@@ -95,6 +105,14 @@ const Border = styled.div`
     border: 1px solid #C4C4C4;
 `
 
+const Table = styled.div`
+    border-radius: 10px 10px 0px 0px;
+    padding: 15px 0;
+    font-size: 13px;
+    font-weight: bold;
+    background: #F4F4F4;
+`
+
 function TeamDesc(){
     const list = MockData();
     const Length = list.length
@@ -104,36 +122,58 @@ function TeamDesc(){
                     Teams Page Description
                 </div>
 
-            <Spann style={{marginLeft:"10%", marginBottom:"20px"}}>
+            <Spann style={{marginLeft:"10%", marginBottom:"20px", width:"20%"}}>
                 <EditDescButton>Edit description</EditDescButton>
                 <TeamButton style={{padding:"0 0.5rem"}}>Preview</TeamButton>
             </Spann>
-    <Container>
-        <OuterSpan>
-        <h1>All Teams</h1>
-        <TeamButton>
-            AddTeam
-            <img style={{paddingLeft:"7px",width:"12px"}} src={Add} alt="+"/>
-        </TeamButton>
-        </OuterSpan>
-        {list.map((item,index) =>{
-                return(
-                    <div>
-                <OuterSpan>
-                <Spann>
-                    <h2>{item.teamName}</h2> 
-                    <div>{item.lastUpdated}</div>
-                    
-                </Spann>
-                <button>
-                    Button
-                    <img style={{paddingLeft:"7px",width:"12px"}} src={Edit} alt="edit"/>
-                </button>
-                </OuterSpan>
-                {(Length !== index + 1 ? <Border/> : null)}
-                </div>)}
-                )}
-    </Container>
+        <Container>
+            <OuterSpan>
+                <h1 style={{paddingBottom:"5px"}}>All Teams</h1>
+                <TeamButton>
+                    AddTeam
+                    <img style={{paddingLeft:"7px",width:"12px"}} src={Add} alt="+"/>
+                </TeamButton>
+            </OuterSpan>
+
+
+            <InnerContainer>
+                <Table style={{padding:"0"}}>
+                    <Spann style={{width:"40%"}}>
+                        <div style={{display:"flex"}}>
+                            <h2>Team</h2>
+                            <div style={{display:"flex",flexDirection:"column", margin:"20px 0"}}>
+                                <img style={{paddingLeft:"7px",width:"12px"}} src={ToggleUp} alt="edit"/>
+                                <img style={{paddingLeft:"7px",width:"12px"}} src={ToggleDown} alt="edit"/>
+                            </div>
+                        </div>
+                        <div style={{display:"flex"}}>
+                            <h2>Last Updated</h2>
+                            <div style={{display:"flex",flexDirection:"column", margin:"20px 0"}}>
+                                <img style={{paddingLeft:"7px",width:"12px"}} src={ToggleUp} alt="edit"/>
+                                <img style={{paddingLeft:"7px",width:"12px"}} src={ToggleDown} alt="edit"/>
+                            </div>
+                        </div>
+                    </Spann>
+                </Table>
+                {list.map((item,index) =>{
+                        return(
+                            <div style={{outline: "2px solid #C4C4C4"}}>
+                        <OuterSpan>
+                        <Spann>
+                            <h2>{item.teamName}</h2> 
+                            <div className="date">{item.lastUpdated}</div>
+                            
+                        </Spann>
+                        <button style={{marginRight:"5px"}}>
+                            Button
+                            <img style={{paddingLeft:"7px",width:"12px"}} src={Edit} alt="edit"/>
+                        </button>
+                        </OuterSpan>
+                        {(Length !== index + 1 ? <Border/> : null)}
+                        </div>)}
+                        )}
+            </InnerContainer>
+        </Container>
     </OuterContainer>
     )
 }
