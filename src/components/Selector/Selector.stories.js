e3ff85d31cc0703cf11d0b634f624ec783a1da30
Jeff Ma <jeff-2c@hotmail.com>
2021-02-09 21:47:35 -0500

    Add working implementation for Selector component

--- src/components/Selector/Selector.stories.js
+++ src/components/Selector/Selector.stories.js
@@ -6,6 +6,7 @@ export default {
   component: Selector,
   argTypes: {
     value: { control: 'text' },
+    items: { control: 'array' }
   },
 };
 
@@ -14,9 +15,19 @@ const Template = (args) => <Selector {...args} />;
 export const EmptyValue = Template.bind({});
 EmptyValue.args = {
   value: '',
+  items: []
 };
 
 export const NonEmptyValue = Template.bind({});
 NonEmptyValue.args = {
   value: 'Text a user has entered',
+  items: ['Text a user has entered', 
+    'Other text', 
+    'Geese are our new overlords', 
+    'long text Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    'Test1',
+    'Test2',
+    'Test3',
+    'Test4',
+  ]
 };
--- src/components/Selector/index.js
+++ src/components/Selector/index.js
@@ -1,18 +1,119 @@
 /* eslint-disable no-unused-vars */
 /* remove before merge  ^^^ */
-import React from 'react';
+import React, {useState} from 'react';
+import {styled} from '@material-ui/core/styles';
+import MUISelector from '@material-ui/core/Select';
+import MUIMenuItem from '@material-ui/core/MenuItem';
+import ExpandLessIcon from '@material-ui/icons/ExpandLess';
+import MUIInputBase from '@material-ui/core/InputBase';
+import * as R from 'ramda';
+import { List } from '@material-ui/core';
 
+const PrimarySelector = styled(MUISelector)({
+  borderRadius: 20,
+  width: '100%',
+  zIndex: 100,
+});
+
+const AddField = styled(MUIMenuItem)({
+  display: "flex",
+  justifyContent: "center",
+  color: "#444444",
+  fontWeight: "bold",
+  fontSize: "0.8rem"
+});
+
+const StyledExpandIcon = styled(ExpandLessIcon)({
+  transition: "cubic-bezier(0.4, 0, 1, 1) 0.1s",
+  top: "10px"
+});
+
+// const AddFieldModal
+
+/**Custom Waterloop CMS Selector. The parent component determines what happens when the 
+ * value changes, including updating the value that's currently displayed in the field.
+ * 
+ * @param className Allows for external styles to be applied to the 
+ *                    component using the styled components library className 
+ *                    prop needs to be passed to the parent JSX element.
+ * @param value     The current value of the input.
+ * @param onSelect  Callback to be called each time that the user selects a value.
+ * @param items     An Array of items to be displayed in the selector dropdown.
+ */
 const Selector = ({
-  className, /* Allows for external styles to be applied to the component using the styled components library
-                className prop needs to be passed to the parent JSX element */
-  value, /* The current value of the input */
-  onSelect, /* Callback to be called each time that the user selects a value */
-  items, /* an Array of items to be displayed in the selector dropdown */
+  className,
+  value,
+  onSelect,
+  items,
+  // isOpen,
   /* Add other props here */
 }) => {
+  const [selected, setSelected] = useState(value);  // TODO: abstract out to parent component.
+  // const [open, setOpen] = useState(isOpen ?? false); // TODO: Also abstract this out to parent component.
+  // const [newInput, setNewInput] = useState(""); // This I don't think needs to be abstracted to parent.
+
+  const handleChange = (event) => {
+    if (!!event.target.value) {
+      onSelect && onSelect(event.target.value);
+      setSelected(event.target.value);
+    }
+  }
   return (
     <div className={className}>
-      Implement ME!
+      <PrimarySelector
+        variant="outlined"
+        color="primary"
+        onChange={handleChange}
+        value={selected}
+        IconComponent={({className}) => <StyledExpandIcon className={className} fontSize="large"/>}
+        // open={open}
+        // onOpen={() => setOpen(true)}
+        // onClose={(event) => {
+        //   // TODO: Find out why this keeps on adding blank values.
+        //   // console.log(event.target.value)
+        //   if (!R.isNil(event.target.value) && event.target.value !== "") {
+        //     items.push(newInput);
+        //     setNewInput("");
+        //     setOpen(false);
+        //   }
+        // }}
+        MenuProps={{
+          anchorOrigin: {
+            vertical: 'bottom',
+            horizontal: 'center',
+          },
+          transformOrigin: {
+            vertical: 'top',
+            horizontal: 'center',
+          },
+          getContentAnchorEl: null
+        }}
+        // inputProps={{
+        //   classes: {
+        //     root: {
+        //       '$focused $notchedOutline': {
+        //         borderColor: 'black'
+        //       }
+        //     }
+        //   }
+        // }}
+      >
+        {items.map((item) => (
+          <MUIMenuItem key={item} value={item} divider>
+            {item}
+          </MUIMenuItem>
+        ))}
+        {/* TODO: Implement add field when new ticket out for it: */}
+        {/* <AddField>
+          <MUIInputBase 
+            placeholder={"+ Add New"}
+            onChange={(event) => {
+              setNewInput(event.target.value)
+            }}
+            value={newInput}
+          />
+        </AddField> */}
+      </PrimarySelector>
     </div>
   );
 };
