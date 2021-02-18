c5dfab57f43a484c392415e2bf6ed5cb95f3fe68
Quinn1876 <qhodges0442@gmail.com>
2021-02-17 23:02:56 -0500

    Removed Blue Border

--- src/components/Selector/Selector.stories.js
+++ src/components/Selector/Selector.stories.js
@@ -3,8 +3,10 @@ import Selector from '.';
 import styles from './Selector.module.css';
 import theme from '../../theme';
 import { ThemeProvider } from 'styled-components';
-import { ThemeProvider as MuiThemeProvider, StylesProvider } from '@material-ui/styles';
-
+import {
+  ThemeProvider as MuiThemeProvider,
+  StylesProvider,
+} from '@material-ui/styles';
 
 export default {
   title: 'Selector',
@@ -12,7 +14,7 @@ export default {
   argTypes: {
     value: { control: 'text' },
     items: { control: 'object' },
-    className: { control: 'text' }
+    className: { control: 'text' },
   },
 };
 
@@ -29,21 +31,28 @@ const Template = (args) => (
 export const EmptyValue = Template.bind({});
 EmptyValue.args = {
   value: 0,
-  items: []
+  items: [],
 };
 
 export const NonEmptyValue = Template.bind({});
 NonEmptyValue.args = {
   value: 1,
   items: [
-    {id: 1, text: 'Text a user has entered'}, 
-    {id: 2, text: '(Note that a test class overrides the selector css in storybook)'}, 
-    {id: 3, text: 'Geese are our new overlords'}, 
-    {id: 4, text: 'long text Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum'},
-    {id: 5, text: 'Test1'},
-    {id: 6, text: 'Test2'},
-    {id: 7, text: 'Test3'},
-    {id: 8, text: 'Test4'}
+    { id: 1, text: 'Text a user has entered' },
+    {
+      id: 2,
+      text: '(Note that a test class overrides the selector css in storybook)',
+    },
+    { id: 3, text: 'Geese are our new overlords' },
+    {
+      id: 4,
+      text:
+        'long text Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+    },
+    { id: 5, text: 'Test1' },
+    { id: 6, text: 'Test2' },
+    { id: 7, text: 'Test3' },
+    { id: 8, text: 'Test4' },
   ],
-  className: styles.testClass
+  className: styles.testClass,
 };
--- src/components/Selector/index.js
+++ src/components/Selector/index.js
@@ -1,28 +1,37 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import MUISelector from '@material-ui/core/Select';
 import MUIMenuItem from '@material-ui/core/MenuItem';
 import ExpandLessIcon from '@material-ui/icons/ExpandLess';
 
-const PrimarySelector = styled(MUISelector)({
-  borderRadius: 20,
-  width: '100%',
-  zIndex: 100,
-  '& fieldset': {
-    borderColor: '#c4c4c4'
-  } 
-});
+const PrimarySelector = styled(MUISelector)`
+  border-radius: 20px;
+  width: 100%;
+  z-index: 100;
+  border-color: #c4c4c4;
+  & fieldset {
+    border-color: #c4c4c4;
+  }
+  &.Mui-focused {
+    & .MuiOutlinedInput-notchedOutline {
+      border-color: #c4c4c4;
+    }
+  }
+  .MuiOutlinedInput-notchedOutline {
+    border-color: #c4c4c4;
+  }
+`;
 
-const StyledExpandIcon = styled(ExpandLessIcon)({
-  transition: "cubic-bezier(0.4, 0, 1, 1) 0.1s",
-  top: "10px"
-});
+const StyledExpandIcon = styled(ExpandLessIcon)`
+  transition: cubic-bezier(0.4, 0, 1, 1) 0.1s;
+  top: 10px;
+`;
 
-/**Custom Waterloop CMS Selector. The parent component determines what happens when the 
+/** Custom Waterloop CMS Selector. The parent component determines what happens when the
  * value changes, including updating the value that's currently displayed in the field.
- * 
- * @param className Allows for external styles to be applied to the 
- *                    component using the styled components library className 
+ *
+ * @param className Allows for external styles to be applied to the
+ *                    component using the styled components library className
  *                    prop needs to be passed to the parent JSX element.
  * @param value     The current value of the input.
  * @param onSelect  Callback to be called each time that the user selects a value.
@@ -37,11 +46,12 @@ const Selector = ({
   const [selected, setSelected] = useState(value);
 
   const handleChange = (event) => {
-    if (!!event.target.value) {
-      onSelect && onSelect(event.target.value);
+    if (event.target.value && typeof onSelect === 'function') {
+      onSelect(event.target.value);
       setSelected(event.target.value);
     }
-  }
+  };
+
   return (
     <PrimarySelector
       className={className}
@@ -49,7 +59,7 @@ const Selector = ({
       color="primary"
       onChange={handleChange}
       value={selected}
-      IconComponent={({className: c}) => <StyledExpandIcon className={c} fontSize="large"/>}
+      IconComponent={({ className: c }) => <StyledExpandIcon className={c} fontSize="large"/>}
       MenuProps={{
         anchorOrigin: {
           vertical: 'bottom',
@@ -59,7 +69,7 @@ const Selector = ({
           vertical: 'top',
           horizontal: 'center',
         },
-        getContentAnchorEl: null
+        getContentAnchorEl: null,
       }}
     >
       {items.map((item) => (
