6d128648b9544acafa05ebbf1569a9e1c46d9c97
Rahul Aggarwal <rahulaggarwal1016@gmail.com>
2021-02-24 23:17:42 -0500

    added image uploading

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 import google from './google-auth';
 import postings from './postings';
 import server from './server';
+import formUpload from './formUpload';
 
 export default {
   google: google(server),
   postings: postings(server),
+  formUpload: formUpload(server),
 };
