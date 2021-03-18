74a2ce56cc0a23840014cc893b7a88d46f74fe0e
Jeff Ma <jeff-2c@hotmail.com>
2021-03-18 14:34:38 -0400

    Add basic input boxes (non-functional)

---
+++ src/pages/sponsors/forms/Copies.js
@@ -0,0 +1,20 @@
+export const sponsorsCopies = {
+  NAME_LABEL: "Sponsor name (required)",
+  WEBSITE_LABEL: "Sponsor link (required)",
+  TIER_LABEL: "Sponsor level (required)",
+  START_DATE_LABEL: "Sponsor since (required)",
+  CONTRIBUTIONS_LABEL: "How have we collaborated (required)",
+  LOGO_LABEL: "Sponsor logo (required)",
+  VIDEO_LINK_LABEL: "Sponsor video link (optional)",
+
+  NAME_PLACEHOLDER: "Sponsor name",
+  WEBSITE_PLACEHOLDER: "Sponsor link",
+  TIER_PLACEHOLDER: "Sponsor level",
+  START_DATE_TERM_PLACEHOLDER: "Term",
+  START_DATE_YEAR_PLACEHOLDER: "Year",
+  CONTRIBUTIONS_PLACEHOLDER: "How have we collaborated",
+  VIDEO_LINK_PLACEHOLDER: "Sponsor video link",
+}
+
+// TODO: To be implemented
+// export const descriptionCopies = {};
--- src/pages/sponsors/forms/EditSponsors.js
+++ src/pages/sponsors/forms/EditSponsors.js
@@ -1,18 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
 import styled from 'styled-components';
+import { sponsorsCopies } from './Copies';
+
+import FormContainer from '../../../components/FormContainer';
+import TextInput from '../../../components/TextInput';
+import Selector from '../../../components/Selector';
+import Button from '../../../components/Button';
+import ImagePreview from '../../../components/ImagePreview';
+
+import Grid from '@material-ui/core/Grid';
+
 
 const Container = styled.div`
   margin: ${({ theme }) => theme.pageMargin};
 `;
 
+const GridContainer = styled(Grid)`
+  padding-bottom: 16px;
+`
+
+/** Uses Grids for responsive design. */
 const EditSponsors = () => {
   const { params: { id } } = useRouteMatch();
 
   return (
     <Container id="sponsor-root">
-      {`id is ${id}`}
-      
+      <Button cancel onClick={() => {}}>
+        &#60; Back
+      </Button>
+      <FormContainer title={sponsorsCopies.NAME_LABEL}>
+        <TextInput placeholder={sponsorsCopies.NAME_PLACEHOLDER}/>
+      </FormContainer>
+      <FormContainer title={sponsorsCopies.WEBSITE_LABEL}>
+        <TextInput placeholder={sponsorsCopies.WEBSITE_PLACEHOLDER} />
+      </FormContainer>
+      <FormContainer title={sponsorsCopies.TIER_LABEL}>
+        <Selector value={0} items={[]} onSelect={() => {}} placeholder={sponsorsCopies.TIER_PLACEHOLDER} />
+      </FormContainer>
+      <FormContainer title={sponsorsCopies.START_DATE_LABEL}>
+        <Selector value="" items={[]} onSelect={() => {}} />
+        <Selector value="" items={[]} onSelect={() => {}} />
+      </FormContainer>
+      <FormContainer title={sponsorsCopies.CONTRIBUTIONS_LABEL}>
+        <TextInput multiLine />
+      </FormContainer>
+      <FormContainer title={sponsorsCopies.LOGO_LABEL}>
+        <ImagePreview />
+      </FormContainer>
+      <FormContainer title={sponsorsCopies.VIDEO_LINK_LABEL}>
+        <TextInput />
+      </FormContainer>      
     </Container>
   );
 }
