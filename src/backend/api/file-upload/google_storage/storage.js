da7979b08dbbb82ef22067a28b2155dfbd5e99dd
Brian Gao <bhgao20014@gmail.com>
2023-01-03 16:53:45 -0500

    Change path import

--- src/backend/api/file-upload/google_storage/storage.js
+++ src/backend/api/file-upload/google_storage/storage.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 import { Storage } from '@google-cloud/storage';
-import path from 'path';
 
+var path = require('path');
 const storage = new Storage({
   keyFilename: path.join(__dirname, process.env.KEY_FILE_NAME),
   projectId: process.env.PROJECT_ID,
