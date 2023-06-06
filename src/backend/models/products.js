45b00443892eb3a39dfb96c962853b68c92901b1
Kevin Lau <kevlau82@gmail.com>
2023-06-06 19:37:15 -0400

    added products model with rename props

--- src/backend/models/products.js
+++ src/backend/models/products.js
@@ -1,13 +1,28 @@
+import { parseTimeForResponse, parseTimeFromRequest } from '../utils/db-dates';
 import renameProps from '../utils/rename-props';
 
-export const fromProduct = (product) =>
-  renameProps(product, {
+export const fromProductVariation = (productVariation) =>
+  renameProps(productVariation, {
     variationName: 'variation_name',
+    productId: 'product_id',
     lastUpdated: 'last_updated',
   });
 
-export const toProduct = (product) =>
-  renameProps(product, {
+export const toProductVariation = (productVariation) => ({
+  ...renameProps(productVariation, {
     variation_name: 'variationName',
+    product_id: 'productId',
     last_updated: 'lastUpdated',
-  });
+  }),
+  lastUpdated: parseTimeForResponse(productVariation.last_updated),
+});
+
+export const toProduct = (product) => ({
+  ...renameProps(product, {}),
+  lastUpdated: parseTimeForResponse(product.last_updated),
+});
+
+export const fromProduct = (product) => ({
+  ...renameProps(product, {}),
+  last_updated: parseTimeFromRequest(product.lastUpdated),
+});
