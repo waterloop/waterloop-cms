f1e1e61ab080349e79214abeea3f3abce98e5629
Kevin Lau <kevlau82@gmail.com>
2023-06-18 15:30:15 -0400

    added db ops and tests

---
+++ src/backend/db/products.js
@@ -0,0 +1,145 @@
+import {
+  fromProductVariation,
+  toProductVariation,
+  toProduct,
+  fromProduct,
+} from '../models/products';
+
+const getProducts = (db) => () =>
+  db('merch_products')
+    .then((res) => {
+      return res.map(toProduct);
+    })
+    .catch((err) => {
+      console.error(`Error in getProducts: ${err}`);
+      throw err;
+    });
+
+const getProductVariations = (db) => () =>
+  db('merch_product_variations')
+    .then((res) => {
+      return res.map(toProductVariation);
+    })
+    .catch((err) => {
+      console.error(`Error in getProductVariations: ${err}`);
+      throw err;
+    });
+
+const getProductById = (db) => (id) =>
+  db('merch_products')
+    .where({ id })
+    .then((res) => {
+      return res.map(toProduct);
+    })
+    .catch((err) => {
+      console.error(`Error in getProductById: ${err}`);
+      throw err;
+    });
+
+const getProductVariationsById = (db) => (variationId) =>
+  db('merch_product_variations')
+    .where({ variationId })
+    .then((res) => {
+      return res.map(toProductVariation);
+    })
+    .catch((err) => {
+      console.error(`Error in getProductVariationsById: ${err}`);
+      throw err;
+    });
+
+const addProduct = (db) => (productInfo) =>
+  db('merch_products')
+    .insert(fromProduct(productInfo))
+    .returning('id')
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.error(`Error in addProduct: ${err}`);
+      throw err;
+    });
+
+const addProductVariation = (db) => (productVariationInfo) =>
+  db('merch_product_variations')
+    .insert(fromProductVariation(productVariationInfo))
+    .returning('id')
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.error(`Error in addProductVariation: ${err}`);
+      throw err;
+    });
+
+const updateProductById = (db) => (id, productInfo) =>
+  db('merch_products')
+    .where({
+      id,
+    })
+    .update({
+      ...fromProduct(productInfo),
+    })
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.error(`Error in updateProductById: ${err}`);
+      throw err;
+    });
+
+const updateProductVariationById = (db) => (id, productVariationInfo) =>
+  db('merch_product_variations')
+    .where({
+      id,
+    })
+    .update({
+      ...fromProductVariation(productVariationInfo),
+    })
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.error(`Error in updateProductVariationById: ${err}`);
+      throw err;
+    });
+
+const deleteProductById = (db) => (id) =>
+  db('merch_products')
+    .where({
+      id,
+    })
+    .del()
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.error(`Error in deleteProductById: ${err}`);
+      throw err;
+    });
+
+const deleteProductVariationById = (db) => (id) =>
+  db('merch_product_variations')
+    .where({
+      id,
+    })
+    .del()
+    .then((response) => {
+      return response;
+    })
+    .catch((err) => {
+      console.error(`Error in deleteProductVariationsById: ${err}`);
+      throw err;
+    });
+
+export default (db) => ({
+  getProducts: getProducts(db),
+  getProductVariations: getProductVariations(db),
+  getProductById: getProductById(db),
+  getProductVariationsById: getProductVariationsById(db),
+  addProduct: addProduct(db),
+  addProductVariation: addProductVariation(db),
+  updateProductById: updateProductById(db),
+  updateProductVariationById: updateProductVariationById(db),
+  deleteProductById: deleteProductById(db),
+  deleteProductVariationById: deleteProductVariationById(db),
+});
---
+++ src/backend/tests/integration/products.test.js
@@ -0,0 +1,362 @@
+process.env.NODE_ENV = 'test';
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import app from '../../index';
+import { db } from '../../db';
+
+chai.use(chaiHttp);
+const should = chai.should();
+const expect = chai.expect;
+
+// implement products, below should be product variation
+
+describe('Products Routes', () => {
+  before('migrate', async function () {
+    this.timeout(60 * 1000); // Resetting the DB can take a few seconds
+    await db.migrate.rollback();
+    return db.migrate.latest();
+  });
+
+  beforeEach('reseed', async function () {
+    this.timeout(60 * 1000); // Resetting the DB can take a few seconds
+    return db.seed.run();
+  });
+
+  // Rollback migration again.
+  after(async function () {
+    this.timeout(60 * 1000); // Resetting the DB can take a few seconds
+    return db.migrate.rollback();
+  });
+
+  describe('GET /api/products', () => {
+    it('should return a list of products when called', (done) => {
+      chai
+        .request(app)
+        .get('/api/products')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          res.body.forEach((item) => {
+            expect(item).to.have.keys([
+              'id',
+              'name',
+              'description',
+              'category',
+            ]);
+          });
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/products/', () => {
+    it('should add post to the database if data is in proper format', (done) => {
+      chai
+        .request(app)
+        .post('/api/products')
+        .send({
+          name: 'a test product name',
+          description: 'a test description',
+          category: 'a test category',
+        })
+        .end((err, res) => {
+          if (err) {
+            console.error(err);
+          }
+          expect(res).to.have.status(200);
+          expect(res.body).to.be.an('array');
+          res.body.forEach((item) => {
+            expect(item).to.have.keys([
+              'id',
+              'name',
+              'description',
+              'category',
+            ]);
+          });
+          done();
+        });
+    });
+
+    it('should return 400 with a poorly formed input', (done) => {
+      chai
+        .request(app)
+        .post('/api/products')
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          done();
+        });
+    });
+  });
+
+  describe('DELETE /api/products/:id', () => {
+    it('should delete a product variation with "id" that exists', (done) => {
+      chai
+        .request(app)
+        .del('/api/products/1')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          chai
+            .request(app)
+            .get('/api/products')
+            .end((err2, res2) => {
+              expect(res2).to.have.status(200);
+              res2.body.forEach((item) => {
+                expect(item.id).to.not.eql(1);
+              });
+              done();
+            });
+        });
+    });
+
+    it('should return 404 when an id is not provided', (done) => {
+      chai
+        .request(app)
+        .del('/api/products')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+
+    it('should continue if the id does not exist', (done) => {
+      chai
+        .request(app)
+        .del('/api/products/99999')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('PATCH /api/products/:id', () => {
+    it('should update the entry with "id" to match the new body', async () => {
+      const products = await db('merch_products');
+      return chai
+        .request(app)
+        .patch(`/api/products/${products[0].id}`)
+        .send({
+          name: 'A updated product name',
+          description: 'A updated product description',
+          category: 'A updated product category',
+        })
+        .then((res) => {
+          expect(res).to.have.status(200);
+          return chai
+            .request(app)
+            .get('/api/products')
+            .then((res2) => {
+              console.log('res2', res2.body);
+              expect(res2).to.have.status(200);
+              const patchedItem = res2.body.find(
+                (item) => item.id === products[0].id,
+              );
+              expect(patchedItem).to.have.property(
+                'A updated product name',
+                'A updated product description',
+                'A updated product category',
+              );
+            });
+        });
+    });
+
+    it('should return 404 if an id is not provided', (done) => {
+      chai
+        .request(app)
+        .patch('/api/products')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+
+    it('should return 400 when body is missing', (done) => {
+      chai
+        .request(app)
+        .patch('/api/products/1')
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          done();
+        });
+    });
+  });
+
+  // variations
+
+  describe('GET /api/products/:id/variations', () => {
+    it('should return a list of product variations when called', (done) => {
+      chai
+        .request(app)
+        .get('/api/products/1/variations')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          res.body.forEach((item) => {
+            expect(item).to.have.keys([
+              'id',
+              'variationName',
+              'productId',
+              'price',
+              'stock',
+              'picture',
+              'lastUpdated',
+            ]);
+          });
+          done();
+        });
+    });
+  });
+
+  describe('POST /api/products/:id/variations', () => {
+    it('should add a product varaiation to the db with a well formed input', (done) => {
+      chai
+        .request(app)
+        .post('/api/products/1/variations')
+        .send({
+          productId: 6969,
+          variationName: 'A new product',
+          price: 69,
+          stock: 1,
+          picture: './test',
+          lastUpdated: 1609518840000,
+        })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          chai
+            .request(app)
+            .get('/api/products/1/variations')
+            .end((err, res2) => {
+              expect(res2).to.have.status(200);
+              res2.body.forEach((item) => {
+                expect(item).to.have.keys([
+                  'id',
+                  'variationName',
+                  'productId',
+                  'price',
+                  'stock',
+                  'picture',
+                  'lastUpdated',
+                ]);
+              });
+              console.log(res2.body);
+              expect(res2.body).to.deep.include({
+                productId: 6969,
+                variationName: 'A new product',
+                price: 69,
+                stock: 1,
+                picture: './test',
+                lastUpdated: 1609518840000,
+              });
+              done();
+            });
+        });
+    });
+
+    it('should return 400 with a poorly formed input', (done) => {
+      chai
+        .request(app)
+        .post('/api/products/1/variations')
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          done();
+        });
+    });
+  });
+
+  describe('DELETE /api/products/:id/variations/:productId', () => {
+    it('should delete a product variation with "id" that exists', (done) => {
+      chai
+        .request(app)
+        .del('/api/products/1/variations/1')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          chai
+            .request(app)
+            .get('/api/products/1/variations')
+            .end((err2, res2) => {
+              expect(res2).to.have.status(200);
+              res2.body.forEach((item) => {
+                expect(item.id).to.not.eql(1);
+              });
+              done();
+            });
+        });
+    });
+
+    it('should return 404 when an id is not provided', (done) => {
+      chai
+        .request(app)
+        .del('/api/products/1/variations')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+
+    it('should continue if the id does not exist', (done) => {
+      chai
+        .request(app)
+        .del('/api/products/1/variations/99999')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+
+  describe('PATCH /api/products/:id/variations/:productId', () => {
+    it('should update the entry with "id" to match the new body', async () => {
+      const products = await db('merch_product_variations');
+      return chai
+        .request(app)
+        .patch(`/api/products/${products[0].id}/variations/1`)
+        .send({
+          productId: 1,
+          variationName: 'A new product',
+          price: 69,
+          stock: 1,
+          picture: './test',
+          lastUpdated: 1609518840000,
+        })
+        .then((res) => {
+          expect(res).to.have.status(200);
+          return chai
+            .request(app)
+            .get(`/api/products/${products[0].id}/variations/1`)
+            .then((res2) => {
+              console.log('res2', res2.body);
+              expect(res2).to.have.status(200);
+              const patchedItem = res2.body.find(
+                (item) => item.id === products[0].id,
+              );
+              expect(patchedItem).to.have.property(
+                6969,
+                'A new product',
+                69,
+                1,
+                './test',
+                1609518840000,
+              );
+            });
+        });
+    });
+
+    it('should return 404 if an id is not provided', (done) => {
+      chai
+        .request(app)
+        .patch('/api/products/1/variations')
+        .end((err, res) => {
+          expect(res).to.have.status(404);
+          done();
+        });
+    });
+
+    it('should return 400 when body is missing', (done) => {
+      chai
+        .request(app)
+        .patch('/api/products/1/variations/1')
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          done();
+        });
+    });
+  });
+});
