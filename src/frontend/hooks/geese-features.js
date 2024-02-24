22983a530276909ad4dd0a7f5b0fab26da88c9a8
Kevin Lau <kevlau82@gmail.com>
2024-02-23 21:49:21 -0500

    add geese-features hook

--- src/frontend/hooks/geese-features.js
+++ src/frontend/hooks/geese-features.js
@@ -1,1 +1,31 @@
-import {useEffect} from 'react'
+import { useEffect } from 'react';
+import api from '../api';
+import { useDispatch, useSelector } from 'react-redux';
+import * as geeseFeaturesActions from '../state/geese-features/actions';
+import * as geeseFeaturesSelectors from '../state/geese-features/selectors';
+
+const useGeeseFeatures = () => {
+  const dispatch = useDispatch();
+  const geeseFeatures = useSelector(geeseFeaturesSelectors.allGeeseFeatures).features;
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const geeseFeaturesResponse =
+          await api.geeseFeatures.getGeeseFeatures();
+
+        const newGeeseFeatures = geeseFeaturesResponse.data;
+
+        dispatch(geeseFeaturesActions.updateGeeseFeatures(newGeeseFeatures));
+      } catch (error) {
+        console.log(error);
+      }
+    })();
+  }, [dispatch]);
+
+  return {
+    geeseFeatures,
+  };
+};
+
+export default useGeeseFeatures;
